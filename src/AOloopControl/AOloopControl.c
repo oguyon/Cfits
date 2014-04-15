8ffdcd897afa6a1119610bdbbb553ce73e485e68
oguyon <oliv.guyon@gmail.com>
2014-04-14 17:43:05 -1000

    print mode values 5

--- src/AOloopControl/AOloopControl.c
+++ src/AOloopControl/AOloopControl.c
@@ -1256,7 +1256,7 @@ int AOcompute(long loop)
 	{
 	  data.image[AOconf[loop].ID_cmd1_modes].array.F[m] = 0.0;
 	  for(n=0; n<AOconf[loop].sizeWFS; n++)
-	    data.image[AOconf[loop].ID_cmd1_modes].array.F[m] += cMat[n*AOconf[loop].NBDMmodes+m]*wfsVec[n];
+	    data.image[AOconf[loop].ID_cmd1_modes].array.F[m] += data.image[AOconf[loop].ID_contrM].array.F[n*AOconf[loop].NBDMmodes+m]*data.image[AOconf[loop].ID_WFS2].array.F[n];
 	}	  
 
       printf("CONV MATRIX MULT :  ");
