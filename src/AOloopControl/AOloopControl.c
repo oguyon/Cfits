68f9ee5b27ae7aa798106b87d084dce7a4e36058
oguyon <oliv.guyon@gmail.com>
2014-06-02 12:17:11 -1000

    jun2 04

--- src/AOloopControl/AOloopControl.c
+++ src/AOloopControl/AOloopControl.c
@@ -2288,7 +2288,7 @@ int AOloopControl_printloopstatus(long loop)
 
   printw("loop number %ld\n", loop);
 
-  /*
+  
   if(AOconf[loop].on == 1)
     printw("loop is ON\n");
   else
@@ -2298,7 +2298,7 @@ int AOloopControl_printloopstatus(long loop)
   else
     printw("log is OFF\n");
   printw("Gain = %f   maxlim = %f\n  GPU = %d\n", AOconf[loop].gain, AOconf[loop].maxlimit, AOconf[loop].GPU);
-  */
+  
 
   return(0);
 }
