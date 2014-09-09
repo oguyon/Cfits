71b37b30653475dc357f4b0879e30b2449a6f111
Olivier Guyon <oliv.guyon@gmail.com>
2014-09-08 22:12:44 -1000

    added SCExAO_control

--- src/SCExAO_control/SCExAO_control.c
+++ src/SCExAO_control/SCExAO_control.c
@@ -111,10 +111,10 @@ int SCExAOcontrol_mv_DMstage(long stepXpos, long stepYpos)
     long ABoffset = 200; /// anti-backlash offset - rule: go negative first, and then positive
     int r;
     long delayus = 2000000;
-	long stepX, stepY;
-	
-	stepX = stepXpos - SCExAO_DM_STAGE_Xpos;
-	stepY = stepYpos - SCExAO_DM_STAGE_Ypos;
+    long stepX, stepY;
+
+    stepX = stepXpos - SCExAO_DM_STAGE_Xpos;
+    stepY = stepYpos - SCExAO_DM_STAGE_Ypos;
 
 
     if((fabs(stepX)>500.0)||(fabs(stepY)>500)||(fabs(stepXpos)>1000.0)||(fabs(stepYpos)>1000))
@@ -129,48 +129,55 @@ int SCExAOcontrol_mv_DMstage(long stepXpos, long stepYpos)
             if(stepX>ABoffset)
             {
                 sprintf(command, "dm_stage x push %ld\n", stepX);
+                printf("command : %s\n", command);
                 r = system(command);
                 usleep(delayus);
             }
             else
             {
                 sprintf(command, "dm_stage x push %ld\n", stepX-ABoffset);
+                printf("command : %s\n", command);
                 r = system(command);
                 usleep(delayus);
 
                 sprintf(command, "dm_stage x push %ld\n", ABoffset);
+                printf("command : %s\n", command);
                 r = system(command);
                 usleep(delayus);
             }
-            
+
             SCExAO_DM_STAGE_Xpos += stepX;
         }
 
         if(stepY!=0)
         {
-         if(stepY>ABoffset)
+            if(stepY>ABoffset)
             {
                 sprintf(command, "dm_stage y push %ld\n", stepY);
+                printf("command : %s\n", command);
                 r = system(command);
                 usleep(delayus);
             }
             else
             {
                 sprintf(command, "dm_stage y push %ld\n", stepY-ABoffset);
+                printf("command : %s\n", command);
                 r = system(command);
                 usleep(delayus);
 
                 sprintf(command, "dm_stage y push %ld\n", ABoffset);
+                printf("command : %s\n", command);
                 r = system(command);
                 usleep(delayus);
             }
-           
+
             SCExAO_DM_STAGE_Ypos += stepY;
         }
     }
 
     return(0);
 }
+
 
 
 
