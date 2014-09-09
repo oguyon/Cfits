34b85e2f2f7f5e027a3f042602cc339903ad49ce
Olivier Guyon <oliv.guyon@gmail.com>
2014-09-08 16:25:11 -1000

    added AOloopControl_Measure_WFScam_PeriodicError

--- src/AOloopControl/AOloopControl.c
+++ src/AOloopControl/AOloopControl.c
@@ -2715,11 +2715,11 @@ int AOloopControl_Measure_WFScam_PeriodicError(long loop, long NBframes, long NB
     fp = fopen("wfscampe.txt","w");
     fclose(fp);
 
-    period_start = 1.0*p1 - 50.0;
-    period_end = 1.0*p1 + 50.0;
+    period_start = 1.0*p1 - 20.0;
+    period_end = 1.0*p1 + 20.0;
 
     phacnt = (long*) malloc(sizeof(long)*NBpha);
-    period_step = (period_end-period_start)/100.0;
+    period_step = (period_end-period_start)/500.0;
     for(period=period_start; period<period_end; period += period_step)
     {
         for(kk=0; kk<NBpha; kk++)
