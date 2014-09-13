fe5178a514ea03a09c9d0c67e9af9e03be5cdad2
Olivier Guyon <oliv.guyon@gmail.com>
2014-09-12 16:04:00 -1000

    memory collision bug: fixing

--- src/AOloopControl/AOloopControl.c
+++ src/AOloopControl/AOloopControl.c
@@ -2993,7 +2993,7 @@ int Measure_Resp_Matrix(long loop, long NbAve, float amp, long nbloop, long fDel
     fflush(stdout);
     sprintf(fname, "AOloop%ld.conf", LOOPNUMBER);
     AOloopControl_loadconfigure(LOOPNUMBER, fname, 1);
-    //exit(0);
+    exit(0);
 
     printf("Importing DM response matrix channel shared memory ...\n");
 	fflush(stdout);
