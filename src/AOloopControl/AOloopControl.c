2f8e57862222dacf0c8d9a3366b61ad250f1e9e8
Olivier Guyon <oliv.guyon@gmail.com>
2014-11-02 11:18:51 -1000

    aoloop test

--- src/AOloopControl/AOloopControl.c
+++ src/AOloopControl/AOloopControl.c
@@ -1911,7 +1911,7 @@ int AOloopControl_loadconfigure(long loop, char *config_fname, int mode)
 
 		if((fp=fopen("./conf/conf_loopname.txt","r"))==NULL)
 		{	
-			printf("ERROR: file conf_loopname.txt missing\n");
+			printf("ERROR: file ./conf/conf_loopname.txt missing\n");
 			exit(0);
 		}
 		r = fscanf(fp, "%s", content);
