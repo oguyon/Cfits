ff066885e6afd04f0c78c5b8ad49364b0c1bc91f
Olivier Guyon <oliv.guyon@gmail.com>
2014-11-06 21:30:41 -1000

    added semaphore

--- src/SCExAO_control/SCExAO_control.c
+++ src/SCExAO_control/SCExAO_control.c
@@ -214,9 +214,12 @@ long SCExAOcontrol_Average_image(char *imname, long NbAve, char *IDnameout)
 
     arrayutmp = (unsigned short*) malloc(sizeof(unsigned short)*xysize);
 
+if(data.image[IDcam].sem==1)
+{	printf("Bringing semaphore to 0\n");
+	fflush(stdout);
 	while(sem_trywait(data.image[ID].semptr)==0)
 	{}
-	
+}	
 //	if(data.image[IDcam].sem==1)
 	//	sem_init(data.image[ID].semptr, 0, 0);
 	
