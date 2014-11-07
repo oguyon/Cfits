0fea35eaeba4217d25605d8eaeafc9240b7fc0b4
Olivier Guyon <oliv.guyon@gmail.com>
2014-11-06 21:44:35 -1000

    added semaphore

--- src/SCExAO_control/SCExAO_control.c
+++ src/SCExAO_control/SCExAO_control.c
@@ -232,12 +232,15 @@ long SCExAOcontrol_Average_image(char *imname, long NbAve, char *IDnameout)
 		}
 		else
 			{
-sem_getvalue(data.image[IDcam].semptr, &semval);
-	printf("semaphore value = %d   \n", semval);
-	fflush(stdout);
-					printf("Waiting for semaphore to post .... ");
+			sem_getvalue(data.image[IDcam].semptr, &semval);
+			printf("semaphore value = %d   \n", semval);
 			fflush(stdout);
+			
+			printf("Waiting for semaphore to post .... ");
+			fflush(stdout);
+			
 			sem_wait(data.image[IDcam].semptr);
+			
 			printf(" done\n");
 			fflush(stdout);
 			}
