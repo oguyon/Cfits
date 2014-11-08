b3b01eb2bf426af81e2c914246f308b0e33aa7a7
Olivier Guyon <oliv.guyon@gmail.com>
2014-11-07 16:31:48 -1000

    added saphira SUTR

--- src/SCExAO_control/SCExAO_control.c
+++ src/SCExAO_control/SCExAO_control.c
@@ -803,6 +803,11 @@ int SCExAOcontrol_SAPHIRA_cam_process(char *IDinname, char *IDoutname)
         if(k<kold)
         {
             printf("\n CUBE COMPLETED -> 2D image ready\n");
+            data.image[IDout].md[0].write = 1;
+            memcpy(data.image[IDout].array.F, data.image[ID2dtmp].array.F, sizeof(float)*xysize);
+			data.image[IDout].md[0].cnt0 ++;
+			data.image[IDout].md[0].write = 0;
+			
             cnt0 = 0;
             cnt1 = 0;
             cnt2 = 0;
