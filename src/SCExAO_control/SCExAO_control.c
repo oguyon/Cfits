04fa48360f3727aeaa9cc499a5bfbf25de321aa6
Olivier Guyon <oliv.guyon@gmail.com>
2014-11-10 15:07:12 -1000

    AOloop

--- src/SCExAO_control/SCExAO_control.c
+++ src/SCExAO_control/SCExAO_control.c
@@ -783,7 +783,7 @@ while(file_exist ("stop_PyAlignCam.txt")==0)
 
 int SCExAOcontrol_Pyramid_flattenRefWF(char *WFScam_name)
 {
-	long zimax = 10;
+	long zimax = 5;
 	long zi;
 	long ID;
 	long NBframes = 2000;
@@ -862,6 +862,7 @@ int SCExAOcontrol_Pyramid_flattenRefWF(char *WFScam_name)
 				data.image[IDdm6].array.F[ii] += data.image[IDdm5].array.F[ii];
 				data.image[IDdm5].array.F[ii] = 0.0;
 			}
+		exit(0);
 	}
 	
 	
