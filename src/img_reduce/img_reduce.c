fa74ed6474d0d845f8799731d50208e79be2fc7f
Olivier Guyon <oliv.guyon@gmail.com>
2014-12-21 16:48:39 -1000

    added badpix correction

--- src/img_reduce/img_reduce.c
+++ src/img_reduce/img_reduce.c
@@ -312,7 +312,7 @@ long IMG_REDUCE_cleanbadpix_fast_precompute(char *IDmask_name)
     nearbypix_array_index = (long*) malloc(sizeof(long)*xysize);
     nearbypix_array_dist2 = (float*) malloc(sizeof(float)*xysize);
     nearbypix_array_coeff = (float*) malloc(sizeof(float)*xysize);
-/*
+
     IDbadpix = image_ID(IDmask_name);
     xsize = data.image[IDbadpix].md[0].size[0];
     ysize = data.image[IDbadpix].md[0].size[1];
@@ -334,7 +334,7 @@ long IMG_REDUCE_cleanbadpix_fast_precompute(char *IDmask_name)
 
     badpixclean_NBbadpix = k;
 
-
+/*
 
     badpixclean_array_indexin = (long*) malloc(sizeof(long)*xysize);
     badpixclean_array_indexout = (long*) malloc(sizeof(long)*xysize);
