d14b8d5c7318f275d5fb343b85be222e4f9409ed
Olivier Guyon <oliv.guyon@gmail.com>
2015-01-04 12:17:51 -1000

    reduced cmat to active pixels / actuators

--- src/AOloopControl/AOloopControl.c
+++ src/AOloopControl/AOloopControl.c
@@ -3972,7 +3972,7 @@ int AOcompute(long loop)
 				//	printf("remapping Vector wfs re\n");
 					//fflush(stdout);				
 					for(wfselem_active=0; wfselem_active<AOconf[loop].sizeDM_active; wfselem_active++)
-						data.image[aoconfID_WFS2_active].array.F[act_active] = data.image[aoconfID_WFS2].array.F[WFS_active_map[wfselem_active]];					
+						data.image[aoconfID_WFS2_active].array.F[wfs_active] = data.image[aoconfID_WFS2].array.F[WFS_active_map[wfselem_active]];					
 					data.image[aoconfID_WFS2_active].md[0].cnt0++;
 					//printf("Vector wfs re-mapped\n");
 					//fflush(stdout);
