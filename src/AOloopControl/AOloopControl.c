13a22d08c5e6589c1cf470005f1618ab6e735ec1
oguyon <oliv.guyon@gmail.com>
2015-04-02 19:24:32 -1000

    bream modes into blocks

--- src/AOloopControl/AOloopControl.c
+++ src/AOloopControl/AOloopControl.c
@@ -1278,6 +1278,8 @@ long AOloopControl_mkModes(char *ID_name, long msize, float CPAmax, float deltaC
                                 printf("Removing [%3ld %3ld] from [%3ld %3ld]    ", m0, mblock0, m, mblock);
                                 
                                 value = 0.0;
+                                value0 = 0.0;
+                                value1 = 0.0;
                                 for(ii=0;ii<msize2;ii++)
                                     {
                                         value += data.image[MBLOCK_ID[mblock]].array.F[m*msize2+ii]*data.image[MBLOCK_ID[mblock0]].array.F[m0*msize2+ii];                                        
