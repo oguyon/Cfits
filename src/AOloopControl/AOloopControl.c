aaa74063b364d60a04ba2c3b3a3cce139aeb2821
oguyon <oliv.guyon@gmail.com>
2015-04-02 19:20:22 -1000

    bream modes into blocks

--- src/AOloopControl/AOloopControl.c
+++ src/AOloopControl/AOloopControl.c
@@ -1282,7 +1282,7 @@ long AOloopControl_mkModes(char *ID_name, long msize, float CPAmax, float deltaC
                                     {
                                         value += data.image[MBLOCK_ID[mblock]].array.F[m*msize2+ii]*data.image[MBLOCK_ID[mblock0]].array.F[m0*msize2+ii];                                        
                                     }
-                                printf("%f\n", value);
+                                printf("%g\n", value);
                             }
                     }
             }
