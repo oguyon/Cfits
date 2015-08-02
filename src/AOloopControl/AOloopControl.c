4a145be4c805f317a333d38ada64cc371171c9f0
oguyon <oliv.guyon@gmail.com>
2015-08-01 19:10:02 -1000

    AOloopControl: added latency measure

--- src/AOloopControl/AOloopControl.c
+++ src/AOloopControl/AOloopControl.c
@@ -4828,7 +4828,7 @@ long AOcontrolLoop_TestSystemLatency(char *dmname, char *wfsname)
     double tmp;
     double dtoffset;
 
-    long NBiter = 10;
+    long NBiter = 100;
     long iter;
     
     FILE *fp;
