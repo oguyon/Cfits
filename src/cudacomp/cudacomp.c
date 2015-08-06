e758ab221568da47cd20b8b7db5669e96c02b769
oguyon <oliv.guyon@gmail.com>
2015-08-05 23:20:09 -1000

    sync

--- src/cudacomp/cudacomp.c
+++ src/cudacomp/cudacomp.c
@@ -939,6 +939,8 @@ void *compute_function( void *ptr )
     FILE *fptest;
     long ii;
 
+    
+
     thdata = (THDATA*) ptr;
     device = thdata->thread_no;
     index = thdata->cindex;
@@ -1021,13 +1023,15 @@ void *compute_function( void *ptr )
             printf("%d  GPU %d: compute reference product\n", index, device);
             fflush(stdout);
  
-            alphatmp = cublasSgemv_alpha;
-            betatmp = cublasSgemv_beta;
+//            alphatmp = cublasSgemv_alpha;
+//            betatmp = cublasSgemv_beta;
 
         // MOVE THIS TO CPU AS A SEPARATE THREAD TO AVOID LOOP PAUSE ??
-            cublasSgemv_alpha = 1.0;
-            cublasSgemv_beta = 0.0;
-            stat = cublasSgemv(gpumatmultconf[index].handle[device], CUBLAS_OP_N, gpumatmultconf[index].M, gpumatmultconf[index].Nsize[device], &cublasSgemv_alpha, gpumatmultconf[index].d_cMat[device], gpumatmultconf[index].M, gpumatmultconf[index].d_wfsVec[device], 1, &cublasSgemv_beta, gpumatmultconf[index].d_dmRef[device], 1);
+    //        cublasSgemv_alpha = 1.0;
+    //        cublasSgemv_beta = 0.0;
+            alpharef = 1.0;
+            betaref = 0.0;
+            stat = cublasSgemv(gpumatmultconf[index].handle[device], CUBLAS_OP_N, gpumatmultconf[index].M, gpumatmultconf[index].Nsize[device], &alpharef, gpumatmultconf[index].d_cMat[device], gpumatmultconf[index].M, gpumatmultconf[index].d_wfsVec[device], 1, &betaref, gpumatmultconf[index].d_dmRef[device], 1);
             if (stat != CUBLAS_STATUS_SUCCESS)
             {
                 printf("cublasSgemv returned error code %d, line(%d)\n", stat, __LINE__);
@@ -1041,8 +1045,8 @@ void *compute_function( void *ptr )
                     printf("   CUBLAS_STATUS_EXECUTION_FAILED\n");
                 exit(EXIT_FAILURE);
             }
-            cublasSgemv_alpha = alphatmp;
-            cublasSgemv_beta = betatmp;
+  //          cublasSgemv_alpha = alphatmp;
+  //          cublasSgemv_beta = betatmp;
 
             gpumatmultconf[index].refWFSinit[device] = 1;
 
