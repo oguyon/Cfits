4b33283605bf4abcfd3daaa62f3d1c1e9995e41c
oguyon <oliv.guyon@gmail.com>
2015-09-18 07:54:17 -1000

    sync

--- src/PIAACMCsimul/PIAACMCsimul.c
+++ src/PIAACMCsimul/PIAACMCsimul.c
@@ -3721,7 +3721,7 @@ double PIAACMCsimul_computePSF(float xld, float yld, long startelem, long endele
 
 
             printf("COMPUTING UNRESOLVED SOURCE PSF -- [%f x %f]\n", xld, yld);
-            printf("Peak constrast (rough estimate)= %g\n", peakcontrast/size/size/optsyst[0].flux[0]/focscale/focscale/normcoeff/normcoeff);
+            printf("Peak constrast (rough estimate)= %g -> %g\n", peakcontrast, peakcontrast/size/size/optsyst[0].flux[0]/focscale/focscale/normcoeff/normcoeff);
             printf("Total light in scoring field = %g  -> Average contrast = %g\n", value, value/(arith_image_total("scoringmask")*focscale*focscale*normcoeff*normcoeff));
 
             if(outsave==1)
