9124c4a6dda73098ae5636854fcdf939b5009d0e
oguyon <oliv.guyon@gmail.com>
2015-09-18 20:33:32 -1000

    sync

--- src/PIAACMCsimul/PIAACMCsimul.c
+++ src/PIAACMCsimul/PIAACMCsimul.c
@@ -1245,7 +1245,7 @@ void PIAACMCsimul_init( OPTPIAACMCDESIGN *design, long index, double TTxld, doub
             printf("Saved piaar1zsag to %s\n", fname);
           */
         optsyst[0].elemtype[elem] = 4;
-        optsyst[0].ASPHSURFMarray[2].surfID = image_ID("piaar1zsag"); //IDpiaar0zsag;
+        optsyst[0].ASPHSURFRarray[2].surfID = image_ID("piaar1zsag"); //IDpiaar0zsag;
         optsyst[0].ASPHSURFRarray[optsyst[0].elemarrayindex[elem]].mat0 = 100; // vacuum
         optsyst[0].ASPHSURFRarray[optsyst[0].elemarrayindex[elem]].mat1 = design[0].PIAAmaterial_code;
     }
