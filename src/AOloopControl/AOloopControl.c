bad80415b552e02aaa617c5916083fb901565c05
oguyon <oliv.guyon@gmail.com>
2015-10-28 12:12:42 -1000

    sync

--- src/AOloopControl/AOloopControl.c
+++ src/AOloopControl/AOloopControl.c
@@ -1860,7 +1860,7 @@ long AOloopControl_mkModes(char *ID_name, long msizex, long msizey, float CPAmax
         }
 
 
-// time : 0:42
+// time : 00:42
 
         /// STEP 3: REMOVE NULL SPACE WITHIN EACH BLOCK - USE SVDlim0 FOR CUTOFF -> fmodes1all.fits
         for(mblock=0; mblock<NBmblock; mblock++)
@@ -1916,8 +1916,8 @@ long AOloopControl_mkModes(char *ID_name, long msizex, long msizey, float CPAmax
         save_fits("fmodes1all", "!./mkmodestmp/fmodes1all.fits");
 
 
-printf("LINE %d\n", __LINE__);
-exit(0);
+
+// time: 00:58
 
 
 
@@ -2027,7 +2027,9 @@ exit(0);
 
     }
 
-    
+    printf("LINE %d\n", __LINE__);
+exit(0);
+
 
 // 1:17
 
