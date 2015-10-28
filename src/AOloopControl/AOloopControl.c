1361e659fecd450a429c51ac8f008a69a89d813a
oguyon <oliv.guyon@gmail.com>
2015-10-28 01:31:50 -1000

    sync

--- src/AOloopControl/AOloopControl.c
+++ src/AOloopControl/AOloopControl.c
@@ -4768,9 +4768,9 @@ int AOloopControl_loadconfigure(long loop, int mode, int level)
                 sprintf(name, "aol%ld_contrMcact%02ld_00", loop, kk);
                 sprintf(fname, "conf/%s.fits", name);
              //   sprintf(fname, "conf/aol%ld_contrMcact%02ld_00", loop, kk);
-                printf("LOADING %s to %s  size %ld %ld\n", fname, name,  AOconf[loop].activeWFScnt, AOconf[loop].activeDMcnt);
+                //printf("LOADING %s to %s  size %ld %ld\n", fname, name,  AOconf[loop].activeWFScnt, AOconf[loop].activeDMcnt);
                 ID = AOloopControl_2Dloadcreate_shmim(name, fname, AOconf[loop].activeWFScnt, AOconf[loop].activeDMcnt);
-                sleep(5);
+                
                if(kk==0)
                     for(ii=0;ii<AOconf[loop].activeWFScnt*AOconf[loop].activeDMcnt;ii++)
                         data.image[aoconfID_contrMcact[0]].array.F[ii] = 0.0;
