e1081378a06f288f81d0d74dc99c811b10e00ded
oguyon <oliv.guyon@gmail.com>
2015-10-31 21:17:49 -1000

    sync

--- src/AOloopControl/AOloopControl.c
+++ src/AOloopControl/AOloopControl.c
@@ -6500,10 +6500,10 @@ int AOloopControl_ProcessZrespM(long loop, char *zrespm_name, char *WFSref0_name
                 printf("ERROR: cannot allocate pixvalarray, size = %ld x %ld\n", NBmat, sizeDM);
                 exit(0);
             }
-        
+/*        
         for(act=0; act<sizeDM; act++)
             for(kmat=0; kmat<NBmat; kmat++)
-                pixvalarray[kmat*sizeDM+act] = 1.0; // data.image[IDWFSrefc_array[kmat]].array.F[act*sizeWFS+ii] ;
+                pixvalarray[kmat*sizeDM+act] = data.image[IDWFSrefc_array[kmat]].array.F[act*sizeWFS+ii] ;
 
         quick_sort_float(pixvalarray, kmat*NBmat);
 
@@ -6512,7 +6512,7 @@ int AOloopControl_ProcessZrespM(long loop, char *zrespm_name, char *WFSref0_name
             ave += pixvalarray[k];
         ave /= (kmax-kmin);
         data.image[IDWFSref].array.F[ii] = ave;
-        
+  */      
         printf("free pixvalarray : %ld x %ld\n", NBmat, sizeDM);
         fflush(stdout);
         free(pixvalarray);
