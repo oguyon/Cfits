6bb7ef4405305ecc1e0ab67c109d013eca057d8a
oguyon <oliv.guyon@gmail.com>
2016-02-14 17:06:49 -1000

    sync

--- src/cudacomp/cudacomp.c
+++ src/cudacomp/cudacomp.c
@@ -1261,7 +1261,7 @@ int GPUcomp_test(long NBact, long NBmodes, long WFSsize, long GPUcnt)
     clock_gettime(CLOCK_REALTIME, &tnow);
     time1sec = 1.0*((long) tnow.tv_sec) + 1.0e-9*tnow.tv_nsec;
 
-    for(iter=0; iter<NBiter; iter++)
+  //  for(iter=0; iter<NBiter; iter++)
         GPU_loop_MultMat_execute(0, &status, &GPUstatus[0], 1.0, 0.0);
 
     clock_gettime(CLOCK_REALTIME, &tnow);
