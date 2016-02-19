680d111e90aabaa909bdd896e7d4aa9ea300a4c7
oguyon <oliv.guyon@gmail.com>
2016-02-18 16:00:42 -1000

    sync

--- src/cudacomp/cudacomp.c
+++ src/cudacomp/cudacomp.c
@@ -1692,7 +1692,7 @@ int GPUcomp_test(long NBact, long NBmodes, long WFSsize, long GPUcnt)
                     val = 0.0;
                     for(k=0;k<n;k++)
                         val += data.image[ID_R].array.F[k*m+ii] * data.image[ID_C].array.F[jj*n+k];
-                    data.image[SVDcheck].array.F[jj*m+ii] = val;
+                    data.image[ID].array.F[jj*m+ii] = val;
                 }
         save_fits("SVDcheck", "!SVDcheck.fits");
     }
