76a396f63d6d5b26e3213d03b7dd7230183a79e3
oguyon <oliv.guyon@gmail.com>
2016-02-20 14:53:05 -1000

    sync

--- src/AOloopControl/AOloopControl.c
+++ src/AOloopControl/AOloopControl.c
@@ -1365,6 +1365,300 @@ long AOloopControl_CrossProduct(char *ID1_name, char *ID2_name, char *IDout_name
 
 
 
+
+
+
+
+// make low order DM modes
+long AOloopControl_mkloDMmodes(char *ID_name, long msizex, long msizey, float CPAmax, float deltaCPA, double xc, double yc, double r0, double r1, int MaskMode)
+{
+    long IDmask;
+    long ID, ID0, IDtm, IDg, IDeModes, IDem, IDtmp, IDtmpg, IDslaved;
+    long ii, jj;
+    double x, y, r, PA, val1, xc1, yc1, ave, totm, totvm, val0, offset, rms, sigma;
+
+    double a0=0.88;
+    double b0=40.0;
+    double a1=1.2;
+    double b1=12.0;
+
+    long NBZ, m;
+    long zindex[10];
+    double zcpa[10];  /// CPA for each Zernike (somewhat arbitrary... used to sort modes in CPA)
+    long IDfreq, IDmfcpa;
+    long k;
+
+    double coeff;
+    long kelim = 20;
+    long kernsize, citer, conviter;
+   long NBciter = 200;
+long NBconviter = 10;
+
+    zindex[0] = 1; // tip
+    zcpa[0] = 0.0;
+
+    zindex[1] = 2; // tilt
+    zcpa[1] = 0.0;
+
+    zindex[2] = 4; // focus
+    zcpa[2] = 0.25;
+
+    zindex[3] = 3; // astig
+    zcpa[3] = 0.4;
+
+    zindex[4] = 5; // astig
+    zcpa[4] = 0.4;
+
+    zindex[5] = 7; // coma
+    zcpa[5] = 0.6;
+
+    zindex[6] = 8; // coma
+    zcpa[6] = 0.6;
+
+    zindex[7] = 6; // trefoil
+    zcpa[7] = 1.0;
+
+    zindex[8] = 9; // trefoil
+    zcpa[8] = 1.0;
+
+    zindex[9] = 12;
+    zcpa[9] = 1.5;
+
+
+
+
+    IDmask = image_ID("dmmask");
+    if(IDmask==-1)
+    {
+        IDmask = create_2Dimage_ID("dmmask", msizex, msizey);
+        for(ii=0; ii<msizex; ii++)
+            for(jj=0; jj<msizey; jj++)
+            {
+                x = 1.0*ii-xc;
+                y = 1.0*jj-yc;
+                r = sqrt(x*x+y*y)/r1;
+                val1 = 1.0-exp(-pow(a1*r,b1));
+                r = sqrt(x*x+y*y)/r0;
+                val0 = exp(-pow(a0*r,b0));
+                data.image[IDmask].array.F[jj*msizex+ii] = val0*val1;
+            }
+        save_fits("dmmask", "!dmmask.fits");
+        xc1 = xc;
+        yc1 = yc;
+    }
+    else /// extract xc and yc from mask
+    {
+        xc1 = 0.0;
+        yc1 = 0.0;
+        totm = 0.0;
+        for(ii=0; ii<msizex; ii++)
+            for(jj=0; jj<msizey; jj++)
+            {
+                xc1 += 1.0*ii*data.image[IDmask].array.F[jj*msizex+ii];
+                yc1 += 1.0*jj*data.image[IDmask].array.F[jj*msizex+ii];
+                totm += data.image[IDmask].array.F[jj*msizex+ii];
+            }
+        xc1 /= totm;
+        yc1 /= totm;
+    }
+
+    totm = arith_image_total("dmmask");
+    if((msizex != data.image[IDmask].md[0].size[0])||(msizey != data.image[IDmask].md[0].size[1]))
+    {
+        printf("ERROR: file dmmask size (%ld %ld) does not match expected size (%ld %ld)\n", data.image[IDmask].md[0].size[0], data.image[IDmask].md[0].size[1], msizex, msizey);
+        exit(0);
+    }
+
+
+    NBZ = 5; /// 3: tip, tilt, focus
+    NBZ = 0;
+    for(m=0; m<10; m++)
+    {
+        if(zcpa[m]<CPAmax)
+            NBZ++;
+    }
+
+
+
+    linopt_imtools_makeCPAmodes("CPAmodes", msizex, CPAmax, deltaCPA, 0.5*msizex, 1.2, 0);
+    ID0 = image_ID("CPAmodes");
+    IDfreq = image_ID("cpamodesfreq");
+
+
+
+    printf("  %ld %ld %ld\n", msizex, msizey, data.image[ID0].md[0].size[2]-1 );
+    ID = create_3Dimage_ID(ID_name, msizex, msizey, data.image[ID0].md[0].size[2]-1+NBZ);
+
+    IDmfcpa = create_2Dimage_ID("modesfreqcpa", data.image[ID0].md[0].size[2]-1+NBZ, 1);
+
+    /*** Create TTF first */
+    zernike_init();
+    for(k=0; k<NBZ; k++)
+    {
+        data.image[IDmfcpa].array.F[k] = zcpa[k];
+        for(ii=0; ii<msizex; ii++)
+            for(jj=0; jj<msizey; jj++)
+            {
+                x = 1.0*ii-xc1;
+                y = 1.0*jj-yc1;
+                r = sqrt(x*x+y*y)/r1;
+                PA = atan2(y,x);
+                data.image[ID].array.F[k*msizex*msizey+jj*msizex+ii] = Zernike_value(zindex[k], r, PA);
+            }
+    }
+    for(k=0; k<data.image[ID0].md[0].size[2]-1; k++)
+    {
+        data.image[IDmfcpa].array.F[k+NBZ] = data.image[IDfreq].array.F[k+1];
+        for(ii=0; ii<msizex*msizey; ii++)
+            data.image[ID].array.F[(k+NBZ)*msizex*msizey+ii] = data.image[ID0].array.F[(k+1)*msizex*msizey+ii];
+    }
+
+
+    for(k=0; k<data.image[ID0].md[0].size[2]-1+NBZ; k++)
+    {
+        /// Remove excluded modes
+        IDeModes = image_ID("emodes");
+        if(IDeModes!=-1)
+        {
+            IDtm = create_2Dimage_ID("tmpmode", msizex, msizey);
+
+            for(ii=0; ii<msizex*msizey; ii++)
+                data.image[IDtm].array.F[ii] = data.image[ID].array.F[k*msizex*msizey+ii];
+            linopt_imtools_image_fitModes("tmpmode", "emodes", "dmmask", 1.0e-5, "lcoeff", 0);
+            linopt_imtools_image_construct("emodes", "lcoeff", "em00");
+            delete_image_ID("lcoeff");
+            IDem = image_ID("em00");
+
+            coeff = 1.0-exp(-pow(1.0*k/kelim,6.0));
+            if(k>2.0*kelim)
+                coeff = 1.0;
+            for(ii=0; ii<msizex*msizey; ii++)
+                data.image[ID].array.F[k*msizex*msizey+ii] = data.image[IDtm].array.F[ii] - coeff*data.image[IDem].array.F[ii];
+
+            delete_image_ID("em00");
+            delete_image_ID("tmpmode");
+        }
+
+
+        ave = 0.0;
+        totvm = 0.0;
+        for(ii=0; ii<msizex*msizey; ii++)
+        {
+            //	  data.image[ID].array.F[k*msize*msize+ii] = data.image[ID0].array.F[(k+1)*msize*msize+ii];
+            totvm += data.image[ID].array.F[k*msizex*msizey+ii]*data.image[IDmask].array.F[ii];
+        }
+        offset = totvm/totm;
+
+        for(ii=0; ii<msizex*msizey; ii++)
+        {
+            data.image[ID].array.F[k*msizex*msizey+ii] -= offset;
+            data.image[ID].array.F[k*msizex*msizey+ii] *= data.image[IDmask].array.F[ii];
+        }
+
+        offset = 0.0;
+        for(ii=0; ii<msizex*msizey; ii++)
+            offset += data.image[ID].array.F[k*msizex*msizey+ii];
+
+        rms = 0.0;
+        for(ii=0; ii<msizex*msizey; ii++)
+        {
+            data.image[ID].array.F[k*msizex*msizey+ii] -= offset/msizex/msizey;
+            rms += data.image[ID].array.F[k*msizex*msizey+ii]*data.image[ID].array.F[k*msizex*msizey+ii];
+        }
+        rms = sqrt(rms/totm);
+        printf("Mode %ld   RMS = %lf\n", k, rms);
+        for(ii=0; ii<msizex*msizey; ii++)
+            data.image[ID].array.F[k*msizex*msizey+ii] /= rms;
+    }
+
+
+    for(k=0; k<data.image[ID0].md[0].size[2]-1+NBZ; k++)
+    {
+        rms = 0.0;
+        for(ii=0; ii<msizex*msizey; ii++)
+        {
+            data.image[ID].array.F[k*msizex*msizey+ii] -= offset/msizex/msizey;
+            rms += data.image[ID].array.F[k*msizex*msizey+ii]*data.image[ID].array.F[k*msizex*msizey+ii];
+        }
+        rms = sqrt(rms/totm);
+        printf("Mode %ld   RMS = %lf\n", k, rms);
+    }
+
+
+
+    if(MaskMode==1)
+    {
+        kernsize = 5;
+        if(2*kernsize>msizex)
+            kernsize = msizex/2;
+        for(citer=0; citer<NBciter; citer++)
+        {
+            printf("Convolution [%3ld/%3ld]\n", citer, NBciter);
+            gauss_filter(ID_name, "modeg", 4.0*pow(1.0*(NBciter-citer)/NBciter,0.5), kernsize);
+            IDg = image_ID("modeg");
+            for(k=0; k<data.image[ID].md[0].size[2]; k++)
+            {
+                for(ii=0; ii<msizex*msizey; ii++)
+                    if(data.image[IDmask].array.F[ii]<0.98)
+                        data.image[ID].array.F[k*msizex*msizey+ii] = data.image[IDg].array.F[k*msizex*msizey+ii];
+            }
+            delete_image_ID("modeg");
+        }
+    }
+
+
+
+
+
+
+
+    /// SLAVED ACTUATORS
+    IDslaved = image_ID("dmslaved");
+    ID = image_ID(ID_name);
+    if((IDslaved != -1)&&(IDmask!=-1))
+    {
+        IDtmp = create_2Dimage_ID("_tmpinterpol", msizex, msizey);
+        for(m=0; m<data.image[ID].md[0].size[2]; m++)
+        {
+            for(ii=0; ii<msizex*msizey; ii++)
+                data.image[IDtmp].array.F[ii] = data.image[ID].array.F[m*msizex*msizey+ii];
+
+            for(conviter=0; conviter<NBconviter; conviter++)
+            {
+                sigma = 0.5*NBconviter/(1.0+conviter);
+                gauss_filter("_tmpinterpol", "_tmpinterpolg", 1.0, 2);
+                IDtmpg = image_ID("_tmpinterpolg");
+                for(ii=0; ii<msizex*msizey; ii++)
+                {
+                    if((data.image[IDmask].array.F[ii]>0.5)&&(data.image[IDslaved].array.F[ii]<0.5))
+                        data.image[IDtmp].array.F[ii] = data.image[ID].array.F[m*msizex*msizey+ii];
+                    else
+                        data.image[IDtmp].array.F[ii] = data.image[IDtmpg].array.F[ii];
+                }
+                delete_image_ID("_tmpinterpolg");
+            }
+            for(ii=0; ii<msizex*msizey; ii++)
+                if(data.image[IDmask].array.F[ii]>0.5)
+                    data.image[ID].array.F[m*msizex*msizey+ii] = data.image[IDtmp].array.F[ii];
+        }
+        delete_image_ID("_tmpinterpol");
+    }
+
+
+
+
+
+
+
+return(ID);
+}
+
+
+
+
+
+
+
 /*** \brief creates AO control modes
  *
  *
@@ -1598,11 +1892,21 @@ long AOloopControl_mkModes(char *ID_name, long msizex, long msizey, float CPAmax
     else
         totm = 1.0;
 
+
+
+
     if(BlockNB<0)
     {
 
         if(MODAL==0)
         {
+            AOloopControl_mkloDMmodes(ID_name, msizex, msizey, CPAmax, deltaCPA, xc, yc, r0, r1, MaskMode);
+            printf("SAVING MODES : %s...\n", ID_name);
+            save_fits(ID_name, "!./mkmodestmp/test_fmodes0all.fits");
+            printf("DONE SAVING\n");
+            exit(0);
+            
+            
             NBZ = 5; /// 3: tip, tilt, focus
 
 
@@ -1804,7 +2108,6 @@ long AOloopControl_mkModes(char *ID_name, long msizex, long msizey, float CPAmax
         save_fits(ID_name, "!./mkmodestmp/test_fmodes0all.fits");
         printf("DONE SAVING\n");
     
-    exit(0);
 // time : 0:04
 
 
