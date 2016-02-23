de008a4d41b8432063594cea04e050d544c8fa98
oguyon <oliv.guyon@gmail.com>
2016-02-22 22:23:55 -1000

    sync

--- src/AOloopControl/AOloopControl.c
+++ src/AOloopControl/AOloopControl.c
@@ -8838,7 +8838,7 @@ int AOloopControl_CompModes_loop(char *ID_CM_name, char *ID_WFSref_name, char *I
     
     
 //    for(iter=0; iter<NBiter; iter++)
-
+ 
     while(1==1)
         {
             #ifdef HAVE_CUDA
@@ -8859,7 +8859,7 @@ int AOloopControl_CompModes_loop(char *ID_CM_name, char *ID_WFSref_name, char *I
             GPU_loop_MultMat_execute(0, &status, &GPUstatus[0], 1.0, 0.0);
             
             for(m=0;m<NBmodes;m++)
-                data.image[ID_coeff].array.F[m] = 1.0; //-= data.image[IDcoeff0].array.F[m];
+                data.image[ID_coeff].array.F[m]-= data.image[IDcoeff0].array.F[m];
             
             #endif
         }
