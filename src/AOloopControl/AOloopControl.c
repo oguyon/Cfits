e229c4ee21b7dc57e71e46c890c16156042aab75
oguyon <oliv.guyon@gmail.com>
2016-02-23 00:00:53 -1000

    sync

--- src/AOloopControl/AOloopControl.c
+++ src/AOloopControl/AOloopControl.c
@@ -8871,8 +8871,8 @@ int AOloopControl_CompModes_loop(char *ID_CM_name, char *ID_WFSref_name, char *I
     
             GPU_loop_MultMat_execute(2, &status, &GPUstatus[0], 1.0, 0.0);
             
-            for(m=0;m<NBmodes;m++)
-                data.image[ID_coeff].array.F[m] = data.image[ID_coeff].array.F[m];
+//            for(m=0;m<NBmodes;m++)
+//              data.image[ID_coeff].array.F[m] = data.image[ID_coeff].array.F[m];
             
             
             iter++;
