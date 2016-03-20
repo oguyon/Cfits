701bb610c674682b34f1846bf728744158c4c8ed
oguyon <oliv.guyon@gmail.com>
2016-03-20 01:08:37 -1000

    sync

--- src/AOloopControl/AOloopControl.c
+++ src/AOloopControl/AOloopControl.c
@@ -9309,8 +9309,8 @@ long AOloopControl_mkPredictiveFilter(char *IDtrace_name, long mode, double dela
         //for(l=0; l<filtsize-4; l++)
         l = 5;
         data.image[IDmatA].array.F[l*NBmvec+m] = data.image[IDtrace].array.F[NBtraceVec*mode + (m+l)];
-         l = 6;
-        data.image[IDmatA].array.F[l*NBmvec+m] = data.image[IDtrace].array.F[NBtraceVec*mode + (m+l)];
+  //       l = 6;
+   //     data.image[IDmatA].array.F[l*NBmvec+m] = data.image[IDtrace].array.F[NBtraceVec*mode + (m+l)];
     }
     fclose(fp);
     
