185fc9328d6855a80d2918d5a277ea79bd809819
oguyon <oliv.guyon@gmail.com>
2016-03-22 19:21:40 -1000

    sync

--- src/AOloopControl/AOloopControl.c
+++ src/AOloopControl/AOloopControl.c
@@ -2820,10 +2820,10 @@ long AOloopControl_mkModes(char *ID_name, long msizex, long msizey, float CPAmax
 
                             LOcoeff = 1.0;
 
-                            LOcoeff = 1.0/(1.0+pow(res/1.0e-1,2.0));
+                            LOcoeff = 1.0/(1.0+pow(10.0*res, 4.0));
 
                             if(res1>1.0)
-                                LOcoeff *= 1.0/pow(res1, 2.0);
+                                LOcoeff *= 1.0/(1.0+pow((res1-1.0)*0.3, 2.0));
 
 
                             fprintf(fpLOcoeff, "%5ld   %20g  %20g   ->  %f\n", m, res, res1, LOcoeff);
