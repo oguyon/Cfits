fa25fdd94184edbc732c7cd11dedbbaad934665e
oguyon <oliv.guyon@gmail.com>
2016-08-13 05:20:53 -1000

    sync

--- src/AOloopControl/AOloopControl.c
+++ src/AOloopControl/AOloopControl.c
@@ -9854,6 +9854,9 @@ int AOloopControl_GPUmodecoeffs2dm_filt_loop(char *modecoeffs_name, char *modeva
 	
 	while(1==1)
         {
+			printf("waiting on %s (%d)\n", modecoeffs_name, semTrigg);
+			fflush(Stdout);
+			
 			COREMOD_MEMORY_image_set_semwait(modecoeffs_name, semTrigg);	
 
 			// FILTER MODES
