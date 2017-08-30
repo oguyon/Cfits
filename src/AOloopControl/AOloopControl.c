01705c977c196a804857b6793f300f27443e6535
oguyon <oliv.guyon@gmail.com>
2017-08-29 21:00:50 -1000

    sync

--- src/AOloopControl/AOloopControl.c
+++ src/AOloopControl/AOloopControl.c
@@ -4269,9 +4269,6 @@ long AOloopControl_computeWFSresidualimage(long loop, char *IDalpha_name)
         else
             sem_wait(data.image[IDimWFS0].semptr[3]);
 
-		
-		printf("alpha = %g\n", data.image[IDalpha].array.F[0]);
-		fflush(stdout);
 
         // imWFS0/tot0 - WFSref -> out
 
