da9e91c2cfd16e695ac8aa89637d262c01b82e3a
oguyon <oliv.guyon@gmail.com>
2017-08-29 23:16:17 -1000

    sync

--- src/AOloopControl/AOloopControl.c
+++ src/AOloopControl/AOloopControl.c
@@ -5677,11 +5677,46 @@ int_fast8_t AOloopControl_AUTOTUNE_LIMITS_on()
 
 int_fast8_t AOloopControl_AUTOTUNE_LIMITS_off()
 {
+	int block;
+	int NBblock;
+	
+	
     if(AOloopcontrol_meminit==0)
         AOloopControl_InitializeMemory(1);
 
     AOconf[LOOPNUMBER].AUTOTUNE_LIMITS_ON = 0;
     AOloopControl_perfTest_showparams(LOOPNUMBER);
+    
+    
+   if(aoconfID_limitb == -1)
+    {
+		char imname[200];
+		
+        if(sprintf(imname, "aol%ld_limitb", LOOPNUMBER) < 1)
+            printERROR(__FILE__, __func__, __LINE__, "sprintf wrote <1 char");
+
+        aoconfID_limitb = read_sharedmem_image(imname);
+    }
+
+    NBblock = data.image[aoconfID_limitb].md[0].size[0];
+
+    // Save Limits
+    for(block=0; block<NBblock; block++)
+		{
+			FILE *fp;
+			char fname[200];
+			
+			sprintf(fname, "conf/param_limitb%02d.txt", block);
+			
+			if((fp=fopen(fname, "w"))==NULL)
+				printERROR(__FILE__, __func__, __LINE__, "Cannot open file");
+			else
+			{
+				fprintf(fp, "%7.5f\n", data.image[aoconfID_limitb].array.F[block]);
+			}
+			
+			fclose(fp);
+		}
 
     return 0;
 }
