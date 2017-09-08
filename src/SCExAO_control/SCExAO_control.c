934cd2884f1a9593dd7ee204e0e74242c9a3b039
oguyon <oliv.guyon@gmail.com>
2017-09-07 19:30:21 -1000

    added WFS noise entry

--- src/SCExAO_control/SCExAO_control.c
+++ src/SCExAO_control/SCExAO_control.c
@@ -84,13 +84,13 @@ static long SCExAO_Pcam_Range = 50000;
 
 static float SCExAO_PZT_STAGE_Xpos = -5.0;
 static float SCExAO_PZT_STAGE_Xpos_ref = -5.0;
-static float SCExAO_PZT_STAGE_Xpos_min = -7.0;
-static float SCExAO_PZT_STAGE_Xpos_max = -3.0;
+static float SCExAO_PZT_STAGE_Xpos_min = -9.0;
+static float SCExAO_PZT_STAGE_Xpos_max = -1.0;
 
 static float SCExAO_PZT_STAGE_Ypos = -5.0;
 static float SCExAO_PZT_STAGE_Ypos_ref = -5.0;
-static float SCExAO_PZT_STAGE_Ypos_min = -7.0;
-static float SCExAO_PZT_STAGE_Ypos_max = -3.0;
+static float SCExAO_PZT_STAGE_Ypos_min = -9.0;
+static float SCExAO_PZT_STAGE_Ypos_max = -1.0;
 
 
 
