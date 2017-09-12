7d160bc6cfdb839fb56c74301bbbd53b1db1674a
oguyon <oliv.guyon@gmail.com>
2017-09-11 19:29:41 -1000

    sync

--- src/AOloopControl_perfTest/AOloopControl_perfTest.c
+++ src/AOloopControl_perfTest/AOloopControl_perfTest.c
@@ -2686,10 +2686,13 @@ long AOloopControl_LoopTimer_Analysis(char *IDname, char *fnametxt, char *outfna
 		printf("\r Sample %ld / %ld \n", sp, NBsample);
 		fflush(stdout);
 		
-		ret = fscanf(fptxt, "%ld %ld %ld %lf\n", &frNB, &l1, &l2, &f1); 
+		ret = fscanf(fptxt, "%20:ld %20:ld %20:ld %20:lf\n", &frNB, &l1, &l2, &f1); 
 		cnt0array[sp] = l1;
 		cnt1array[sp] = l2;
 		frameTimearray[sp] = f1;
+		
+		printf("read :   %20ld  %20ld  %20ld  20f\n", frNB, l1, l2, f1);
+		fflush(stdout);
 		
 		//fprintf(fpout, "%5ld  %10lu  %10lu  %18.9lf    ", sp, cnt0array[sp], cnt1array[sp], frameTimearray[sp]);
 		/*
